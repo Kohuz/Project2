f5df13486ecd97adc0dffa2f2e8c9ae7bf296a27
Kohuz <48293618+Kohuz@users.noreply.github.com>
2020-11-24 12:21:42 +0100

    Update sheet2.c
    bettter main

--- sheet2.c
+++ sheet2.c
@@ -30,7 +30,7 @@ row_t row_ctor(int no_of_columns) {
 }
 table_t table_ctor(int no_of_rows) {
     table_t tab;
-    row_t *ptr = malloc(no_of_rows * sizeof(row_t));
+    row_t *ptr = malloc(no_of_rows*sizeof(row_t));
     tab.no_of_rows = no_of_rows;
     tab.row = ptr;
     tab.cap = 1;
@@ -39,49 +39,35 @@ table_t table_ctor(int no_of_rows) {
 
 void table_append(table_t *tab, int line_count, int cell_count){
     row_t row = row_ctor(cell_count);
+    tab->cap += 1;
+    tab->row = realloc(tab->row, tab->cap * sizeof(row_t));
     tab->row[line_count-1] = row;
 }
-
-
-int main(int argc, char* argv[]) {
+void table_allocate(char* argv, char* delim, table_t *tab) {
     FILE* file;
-    FILE* file2;
-    FILE* file3;
+    file = fopen(argv, "r");
     int ch;
-
-    char* delim = " ";
-    int line_count = 1, cell_count = 1;
-    file2 = fopen(argv[1], "r");
-    file = fopen(argv[1], "r");
-    file3 = fopen(argv[1], "r");
-
-
-    line_count = 1;
-
-    while((ch = fgetc(file2)) != EOF) {
-        if(ch == '\n') {
-            line_count++;
-        }
-
-    }
-    table_t tab = table_ctor(line_count);
-
-    line_count = 0;
+    int cell_count = 0, line_count = 0;
     while((ch = fgetc(file)) != EOF) {
         if(strchr(delim,ch))
             cell_count++;
         if(ch == '\n') {
             line_count++;
-            table_append(&tab, line_count, cell_count);
-            tab.row[line_count].no_of_columns = cell_count;
+            table_append(tab, line_count, cell_count);
+            tab->row[line_count].no_of_columns = cell_count;
             cell_count = 1;
         }
 
     }
-    int i = 0;
-    int cur_line = 0, cur_cell = 0;
-    while((ch = fgetc(file3)) != EOF) {
-        tab.row[cur_line].column[cur_cell].content[i] = ch;
+    fclose(file);
+}
+void read_table(char* argv, char* delim,table_t *tab) {
+    FILE* file;
+    file = fopen(argv, "r");
+    int ch;
+    int i = 0, cur_line = 0, cur_cell = 0;
+    while((ch = fgetc(file)) != EOF) {
+        tab->row[cur_line].column[cur_cell].content[i] = ch;
         i++;
         if(strchr(delim,ch)) {
             i = 0;
@@ -94,6 +80,32 @@ int main(int argc, char* argv[]) {
         }
 
     }
+    fclose(file);
+}
+void table_dtor(table_t *tab) {
+    for(int j = 0; j < tab->no_of_rows; j++){
+        free(tab->row[j].column);
+    }
+    free(tab->row);
+
+}
+
+int main(int argc, char* argv[]) {
+    char delim[100] = " ";
+//    int arg_idx = 1;
+//    if(!strcmp(argv[1], "-d")) {
+//        if(argv[2])
+//        {
+//            strcpy(delim, argv[2]);
+//            arg_idx = 3;
+//        }
+//        else {
+//            fprintf(stderr, "Missing delimeter");
+//        }
+//    }
+    table_t tab = table_ctor(1);
+    table_allocate(argv[1], delim, &tab);
+    read_table(argv[1], delim, &tab);
 
     printf("%s\n", tab.row[0].column[0].content);
     printf("%s\n", tab.row[0].column[1].content);
@@ -101,12 +113,12 @@ int main(int argc, char* argv[]) {
     printf("%s\n", tab.row[2].column[0].content);
     printf("%s\n", tab.row[2].column[2].content);
 
-    for(int j = 0; j < tab.no_of_rows; j++){
-        free(tab.row[j].column);
-    }
-    free(tab.row);
+//    for(int j = 0; j < tab.no_of_rows; j++){
+//        free(tab.row[j].column);
+//    }
+//    free(tab.row);
 
-
+    table_dtor(&tab);
 
 
 
@@ -115,8 +127,5 @@ int main(int argc, char* argv[]) {
 
 //    tab = split(argv[1],&file, line_count, cell_count, delim);
 //
-    fclose(file2);
-    fclose(file);
-    fclose(file3);
     return 0;
 }
